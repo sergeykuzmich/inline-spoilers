2c0f0ba071f01a8bd9dcb1a6e60635cd8fed95ae
Sergey Kuzmich <7501301@gmail.com>
2014-05-23 22:22:43 +0300

    Add plugin files

---
+++ inline-spoilers.php
@@ -0,0 +1,56 @@
+<?php
+/*
+Plugin Name: Inline Spoilers
+Plugin URI: http://kuzmi.ch
+Description: The plugin allows to create content spoilers with simple shortcode.
+Version: 1.0
+Author: Sergey Kuzmich
+Author URI: http://kuzmi.ch
+License: GPLv2
+*/
+
+/**
+ * @package Inline Spoilers
+ * @version 1.0
+ * @since 1.0
+ */
+
+add_action( 'plugins_loaded', 'is_load_textdomain' );
+function is_load_textdomain() {
+	load_plugin_textdomain( 'is', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' ); 
+}
+
+add_shortcode ( 'spoiler', 'is_spoiler_shortcode' );
+function is_spoiler_shortcode( $atts, $content ) {
+	extract( shortcode_atts( array (
+		'title' => ''
+	), $atts ) );
+
+	$output  = "<div class=\"spoiler-wrap\">\n";
+	$output .= "<div class=\"spoiler-head\" title=\"". __('Expand', 'is') ."\">\n";
+	$output .= ($title == '')?__('Expand', 'is'):$title;
+	$output .= "\n</div>\n";
+	$output .= "<div class=\"spoiler-body\">\n";
+	$output .= $content;
+	$output .= "\n</div>\n";
+	$output .= "</div>\n";
+
+	return $output;
+}
+
+add_action( 'wp_enqueue_scripts', 'is_styles_scripts' );
+function is_styles_scripts() {
+	global $post;
+	wp_register_style( 'is_style', plugins_url( 'styles/is-styles.css', __FILE__ ), null, '1.0' );
+	wp_register_script( 'is_script', plugins_url( 'scripts/is-scripts.js', __FILE__ ), array( 'jquery' ), '1.0', true );
+
+	if( has_shortcode( $post->post_content, 'spoiler' ) ) {
+		wp_enqueue_style( 'is_style' );
+		wp_enqueue_script( 'is_script' );
+
+		$translation_array = array( 'expand' 	=> __( 'Expand', 'is' ), 
+									'collapse' 	=> __( 'Collapse', 'is' ) );
+
+		wp_localize_script( 'is_script', 'title', $translation_array );
+	}  
+}
---
+++ scripts/is-scripts.js
@@ -0,0 +1,22 @@
+/**
+ * @package Inline Spoilers
+ * @version 1.0
+ * @since 1.0
+ */
+
+jQuery(function(){
+	jQuery(".spoiler-body").hide();
+
+	jQuery(".spoiler-head").on('click', function(event){
+		$this = jQuery(this);
+		if($this.hasClass("expanded")) {
+			$this.removeClass("expanded");
+			$this.next().slideUp("fast");
+			$this.prop('title', title.expand);
+		} else {
+			$this.addClass("expanded");
+			$this.next().slideDown("fast");
+			$this.prop('title', title.collapse);
+		}
+	});
+});
