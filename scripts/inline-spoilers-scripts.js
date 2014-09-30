bf808a0b595b7d6faaec38c6bc53ffd5f02086f4
Sergey Kuzmich <7501301@gmail.com>
2014-09-30 12:29:53 +0300

    JavaScript bug fix

--- scripts/inline-spoilers-scripts.js
+++ scripts/inline-spoilers-scripts.js
@@ -6,11 +6,13 @@ jQuery(function(){
 	jQuery(".spoiler-head").on('click', function(event){
 		$this = jQuery(this);
 		if($this.hasClass("expanded")) {
-			$this.switchClass("expanded", "collapsed");
+			$this.removeClass("expanded");
+			$this.addClass("collapsed");
 			$this.prop('title', title.expand);
 			$this.next().slideUp("fast");
 		} else {
-			$this.switchClass("collapsed", "expanded");
+			$this.removeClass("collapsed");
+			$this.addClass("expanded");
 			$this.prop('title', title.collapse);
 			$this.next().slideDown("fast");
 		}
