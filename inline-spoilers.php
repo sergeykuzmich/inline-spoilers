71e89e6ba2d54c5737b43269b96495012751df5d
Sergey Kuzmich <7501301@gmail.com>
2015-03-18 15:13:28 +0300

    Add WP_DEBUG mode. Fix incorrect paragraph tags inside the spoiler

--- inline-spoilers.php
+++ inline-spoilers.php
@@ -21,29 +21,37 @@ if ( ! defined( 'WPINC' ) ) {
 
 add_action( 'plugins_loaded', 'is_load_textdomain' );
 function is_load_textdomain() {
-	load_plugin_textdomain( 'inline-spoilers', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' ); 
+	load_plugin_textdomain( 'inline-spoilers', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' );
 }
 
-add_shortcode ( 'spoiler', 'is_spoiler_shortcode' );
+add_shortcode( 'spoiler', 'is_spoiler_shortcode' );
 function is_spoiler_shortcode( $atts, $content ) {
-	extract( shortcode_atts( array (
-		'title' => __('Spoiler', 'inline-spoilers'),
+	extract( shortcode_atts( array(
+		'title'         => __( 'Spoiler', 'inline-spoilers' ),
 		'initial_state' => 'collapsed'
 	), $atts ) );
 
-	$title = esc_attr( $title );
-	$head_class = (esc_attr( $initial_state ) == 'collapsed')?' collapsed':' expanded';
+	$title      = esc_attr( $title );
+	$head_class = ( esc_attr( $initial_state ) == 'collapsed' ) ? ' collapsed' : ' expanded';
 
-	$body_atts = (esc_attr( $initial_state ) == 'collapsed')?'style="display: none;"':'style="display: block;"';
+	$body_atts = ( esc_attr( $initial_state ) == 'collapsed' ) ? 'style="display: none;"' : 'style="display: block;"';
 
-	$output  = "<div class=\"spoiler-wrap\">\n";
-	$output .= "<div class=\"spoiler-head".$head_class."\" title=\"". __('Expand', 'inline-spoilers') ."\">\n";
+	$output = "<div class=\"spoiler-wrap\">";
+	$output .= WP_DEBUG ? "\n" : "";
+	$output .= "<div class=\"spoiler-head" . $head_class . "\" title=\"" . __( 'Expand', 'inline-spoilers' ) . "\">";
+	$output .= WP_DEBUG ? "\n" : "";
 	$output .= $title;
-	$output .= "\n</div>\n";
-	$output .= "<div class=\"spoiler-body\" ".$body_atts.">\n";
-	$output .= do_shortcode($content);
-	$output .= "\n</div>\n";
-	$output .= "</div>\n";
+	$output .= WP_DEBUG ? "\n" : "";
+	$output .= "</div>";
+	$output .= WP_DEBUG ? " <!-- .spoiler-head -->\n" : "";
+	$output .= "<div class=\"spoiler-body\" " . $body_atts . ">";
+	$output .= WP_DEBUG ? "\n" : "";
+	$output .= preg_replace( "%^</p>\n(.*)\n<p>$%simx", '$1', do_shortcode( $content ) );
+	$output .= WP_DEBUG ? "\n" : "";
+	$output .= "</div>";
+	$output .= WP_DEBUG ? " <!-- .spoiler-body -->\n" : "";
+	$output .= "</div>";
+	$output .= WP_DEBUG ? " <!-- .spoiler-wrap -->\n" : "";
 
 	return $output;
 }
@@ -54,13 +62,15 @@ function is_styles_scripts() {
 	wp_register_style( 'inline-spoilers_style', plugins_url( 'styles/inline-spoilers-default.css', __FILE__ ), null, '1.0' );
 	wp_register_script( 'inline-spoilers_script', plugins_url( 'scripts/inline-spoilers-scripts.js', __FILE__ ), array( 'jquery' ), '1.0', true );
 
-	if( has_shortcode( $post->post_content, 'spoiler' ) ) {
+	if ( has_shortcode( $post->post_content, 'spoiler' ) ) {
 		wp_enqueue_style( 'inline-spoilers_style' );
 		wp_enqueue_script( 'inline-spoilers_script' );
 
-		$translation_array = array( 'expand' 	=> __( 'Expand', 'inline-spoilers' ), 
-									'collapse' 	=> __( 'Collapse', 'inline-spoilers' ) );
+		$translation_array = array(
+			'expand'   => __( 'Expand', 'inline-spoilers' ),
+			'collapse' => __( 'Collapse', 'inline-spoilers' )
+		);
 
 		wp_localize_script( 'inline-spoilers_script', 'title', $translation_array );
-	}  
+	}
 }
