56c1ff20c04ce73b5fd25814f2024462a861f84e
Sergey Kuzmich <contact@kuzmi.ch>
2017-10-15 23:23:27 +0300

    Debug deployment script

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -3,12 +3,12 @@
 # @Author: Sergey Kuzmich
 # @Date:   2017-10-15 00:47:04
 # @Last Modified by:   Sergey Kuzmich
-# @Last Modified time: 2017-10-15 23:20:48
+# @Last Modified time: 2017-10-15 23:22:33
 
 echo "Deploying tag $TRAVIS_TAG"
 pwd
 
-export SVN_REPOSITORY_DIR="~/inline-spoilers-svn"
+export SVN_REPOSITORY_DIR="../inline-spoilers-svn"
 
 #  1. Clone complete SVN repository to separate directory
 svn co $SVN_REPOSITORY $SVN_REPOSITORY_DIR
