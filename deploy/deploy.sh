64164a2819edb4a6e6379016ef269806ba35265a
Sergey Kuzmich <contact@kuzmi.ch>
2017-10-15 23:31:25 +0300

    Execute deployment

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -3,9 +3,9 @@
 # @Author: Sergey Kuzmich
 # @Date:   2017-10-15 00:47:04
 # @Last Modified by:   Sergey Kuzmich
-# @Last Modified time: 2017-10-15 23:29:56
+# @Last Modified time: 2017-10-15 23:31:04
 
-echo "Deploying tag $TRAVIS_BRANCH"
+echo "Deploying tag $TRAVIS_TAG"
 
 #  1. Clone complete SVN repository to separate directory
 svn co $SVN_REPOSITORY ../inline-spoilers-svn
@@ -32,12 +32,12 @@ rm -rf .travis.yml
 rm -rf README.md
 
 #  9. Create SVN tag directory
-mkdir ../tags/$TRAVIS_BRANCH
+mkdir ../tags/$TRAVIS_TAG
 
 # 10. Copy trunk/ to tags/{tag}/
-cp -R ./* ../tags/$TRAVIS_BRANCH
+cp -R ./* ../tags/$TRAVIS_TAG
 
 # 11. Commit SVN changes
-# svn ci --message "Release $TRAVIS_BRANCH" --username $SVN_USERNAME --password $SVN_PASSWORD --non-interactive
+svn ci --message "Release $TRAVIS_TAG" --username $SVN_USERNAME --password $SVN_PASSWORD --non-interactive
 
-echo "Deployment of $TRAVIS_BRANCH is complete"
+echo "Deployment of $TRAVIS_TAG is complete"
