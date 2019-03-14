c7160330f433fdbd70a4de0af0ed65d489bcf53c
Sergey Kuzmich <contact@kuzmi.ch>
2019-03-14 18:11:40 +0300

    WIP: Setup save method

--- block/index.js
+++ block/index.js
@@ -1,72 +1,86 @@
 ( function( blocks, editor, i18n, element, components, _ ) {
+  var __ = i18n.__;
   var el = element.createElement;
   var RichText = editor.RichText;
 
   blocks.registerBlockType( 'inline-spoilers/blocks', {
-    title: i18n.__( 'Inline Spoiler', 'inline-spoilers' ),
+    title: __( 'Inline Spoiler', 'inline-spoilers' ),
     icon: 'hidden',
     category: 'formatting',
     attributes: {
       title: {
-        type: 'array',
-        source: 'children',
-        selector: 'h2',
+        type: 'string'
       },
       content: {
-        type: 'array',
-        source: 'children',
-        selector: '.spoiler-content',
+        type: 'string',
+        source: 'html'
       },
     },
+
     edit: function( props ) {
-      var attributes = props.attributes;
-      var content = props.attributes.content;
-      function onTitleChange(event) {
+      var { title, content } = props.attributes;
+
+      function onChangeTitle(event) {
         props.setAttributes({title: event.target.value})
       }
 
+      function onChangeContent(value) {
+        props.setAttributes({content: value});
+      }
 
       return (
         el( 'div', { className: props.className },
-         el("div", { class: "spoiler-title" }, 
-          el("label", null, "Title"), 
+         el("div", { class: "spoiler-title" },
+          el("label", null, "Title"),
           el("input", {
             type: "text",
-            placeholder: i18n.__( 'Expand me...', 'inline-spoilers' ),
-            value: attributes.title,
+            placeholder: __( 'Spoiler', 'inline-spoilers' ),
+            value: title,
             class: "spoiler-title",
-            onChange: onTitleChange
+            onChange: onChangeTitle
           })
         ),
           el( RichText, {
-            tagName: 'div',
-            inline: false,
-            placeholder: i18n.__( 'Write spoiler content…', 'inline-spoilers' ),
-            value: attributes.content,
-            onChange: function( value ) {
-              props.setAttributes( { content: value } );
-            },
+            placeholder: __( 'Spoiler content…', 'inline-spoilers' ),
+            value: content,
+            onChange: onChangeContent
           } )
         )
       );
     },
+
     save: function( props ) {
-      var attributes = props.attributes;
+      var title = props.attributes.title;
+      var content = props.attributes.content;
+
+      //<div class="spoiler-wrap">
+      //  <div class="spoiler-head collapsed" title="Expand">asdf</div>
+      //  <div class="spoiler-body" style="display: none;">
+      //    mem<strong>eme</strong>me
+      //  </div>
+      //  <noscript><div class="spoiler-body">mem<strong>eme</strong>me</div></noscript>
+      //</div>
 
       return (
-        el( 'div', { className: props.className },
-          el( RichText.Content, {
-            tagName: 'h2', value: attributes.title
-          } ),
-          el( RichText.Content, {
-            tagName: 'div', className: 'spoiler-content', value: attributes.content
-          } ),
+        el("div", null,
+          el("div", { class: "spoiler-wrap" },
+            el("div", { class: "spoiler-head collapsed", title: "Expand" },
+              title
+            ),
+            el("div", { class: "spoiler-body", style: "display: none;" },
+              el( RichText.Content, { tagName: 'div', value: content })
+            ),
+            el("noscript", null,
+              el("div", { class: "spoiler-body" },
+                el( RichText.Content, { tagName: 'div', value: content })
+              )
+            )
+          )
         )
       );
     },
-  } );
-
-} )(
+  });
+})(
   window.wp.blocks,
   window.wp.editor,
   window.wp.i18n,
@@ -74,75 +88,3 @@
   window.wp.components,
   window._,
 );
-
-
-// ( function( blocks, editor, i18n, element) {
-//   var el = wp.element.createElement;
-//   var RichText = editor.RichText;
-//   var __ = wp.i18n.__;
-
-//   blocks.registerBlockType( 'inline-spoilers/block', {
-//     title: __( 'Inline Spoilers', 'inline-spoilers' ),
-//     icon: 'hidden',
-//     category: 'formatting',
-//     attributes: {
-//       // title: {
-//       //   type: 'string',
-//       // },  
-//       content: {
-//         type: 'array',
-//         source: 'children',
-//         selector: 'p',
-//       },
-//     },
-
-//     edit: function( props ) {
-//       // var attributes = props.attributes;
-//       var content = props.attributes.content;
-//       // function onTitleChange(event) {
-//       //   props.setAttributes({title: event.target.value})
-//       // }
-
-//       function onChangeContent( newContent ) {
-//         props.setAttributes( { content: newContent } );
-//       }
-
-//       // return //el( 'div', { className: props.className },
-//         // el("div", { class: "spoiler-title" }, 
-//         //   el("label", null, "Title"), 
-//         //   el("input", {
-//         //     type: "text",
-//         //     placeholder: __( 'Spoiler', 'inline-spoilers' ),
-//         //     value: attributes.title,
-//         //     class: "spoiler-title",
-//         //     onChange: onTitleChange
-//         //   })
-//         // ),
-//       return  el(
-//           RichText,
-//           {
-//             tagName: 'p',
-//             className: props.className,
-//             onChange: onChangeContent,
-//             value: attributes.content
-//           },
-//           placeholder: __( 'Enter a GitHub Gist URL' ),
-//         );
-//       //);
-//     },
-
-//     save: function(props) {
-//       return 'Code';
-//       // const title = props.attributes.title || '&nbsp;';
-//       // return "[spoiler title=\"" + title + "\"]" + props.attributes.content + "[/spoiler]";
-//     },
-
-
-//   } );
-
-// } )(
-//   window.wp.blocks,
-//   window.wp.editor,
-//   window.wp.i18n,
-//   window.wp.element,
-// );
