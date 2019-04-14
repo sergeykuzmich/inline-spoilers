37c255ed7e505f2bd3edc7ff1e7389dae32443f5
Sergey Kuzmich <contact@kuzmi.ch>
2019-04-14 16:44:17 +0300

    Fix jquery code duplication

--- scripts/inline-spoilers-scripts.js
+++ scripts/inline-spoilers-scripts.js
@@ -7,13 +7,11 @@ jQuery(function () {
     jQuery(".spoiler-head").on('click', function (event) {
         $this = jQuery(this);
         if ($this.hasClass("expanded")) {
-            $this.removeClass("expanded");
-            $this.addClass("collapsed");
+            $this.removeClass("expanded").addClass("collapsed");
             $this.prop('title', title.expand);
             $this.next().slideUp("fast");
         } else {
-            $this.removeClass("collapsed");
-            $this.addClass("expanded");
+            $this.removeClass("collapsed").addClass("expanded");
             $this.prop('title', title.collapse);
             $this.next().slideDown("fast");
         }
