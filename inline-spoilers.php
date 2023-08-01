2bda9972fd3166ba916535fb8a5e902be7906dfc
Sergey Kuzmich <accounts@kuzmi.ch>
2023-08-01 20:51:11 +0200

    Update inline-spoilers.php

--- inline-spoilers.php
+++ inline-spoilers.php
@@ -5,6 +5,7 @@
  * Description:				The plugin allows to create content spoilers with simple shortcode & guttenberg block.
  * Version:						1.5.4
  * Requires at least:	5.2
+ * Tested up to:			6.2.2
  * Requires PHP:			5.6
  * Author:						Sergey Kuzmich
  * Author URI:				http://kuzmi.ch
