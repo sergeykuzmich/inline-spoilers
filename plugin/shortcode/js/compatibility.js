14f7c56684ec6012382ae4f11a50af674f304c59
Sergey Kuzmich <accounts@kuzmi.ch>
2025-02-03 00:21:16 +0100

    chore: Run linter

--- plugin/shortcode/js/compatibility.js
+++ plugin/shortcode/js/compatibility.js
@@ -1,1 +1,2 @@
+/* eslint-disable */
 jQuery(function(){jQuery(".spoiler-head").removeClass("no-icon"),jQuery(".spoiler-head").on("click",function(e){$isExpanded=($this=jQuery(this)).hasClass("expanded"),$this.toggleClass("expanded").toggleClass("collapsed"),$this.prop("title",$isExpanded?"Collapse":"Expand"),$isExpanded?$this.next().slideUp("fast"):$this.next().slideDown("fast")})});
