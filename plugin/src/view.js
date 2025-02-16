a67d03f13d1e641f5963034ba2b3a1ec22797120
Sergey Kuzmich <accounts@kuzmi.ch>
2025-02-16 15:43:25 +0100

    refactor: Improve code formatting and readability

--- plugin/src/view.js
+++ plugin/src/view.js
@@ -1,34 +1,34 @@
 window.inlineSpoilersAnimation = () => {
 	window.inlineSpoilersAnimationInitialized = true;
 
-	document.body.addEventListener('click', (event) => {
+	document.body.addEventListener( 'click', ( event ) => {
 		const spoiler = event.target.closest(
 			'details.wp-block-inline-spoilers-block'
 		);
 
-		if (!spoiler) {
+		if ( ! spoiler ) {
 			return;
 		}
 
 		event.preventDefault();
 
-		const summary = spoiler.querySelector('summary');
-		if (spoiler.open) {
+		const summary = spoiler.querySelector( 'summary' );
+		if ( spoiler.open ) {
 			const border = parseInt(
 				window
-					.getComputedStyle(summary)
-					.getPropertyValue('border-bottom-width')
-					.slice(0, -2)
+					.getComputedStyle( summary )
+					.getPropertyValue( 'border-bottom-width' )
+					.slice( 0, -2 )
 			);
 
-			const to = `${summary.offsetHeight + border}px`;
-			const from = `${spoiler.offsetHeight}px`;
+			const to = `${ summary.offsetHeight + border }px`;
+			const from = `${ spoiler.offsetHeight }px`;
 
 			spoiler.style.overflow = 'hidden';
 
 			const ani = spoiler.animate(
 				{
-					height: [from, to],
+					height: [ from, to ],
 				},
 				{
 					duration: 200,
@@ -38,23 +38,23 @@ window.inlineSpoilersAnimation = () => {
 
 			ani.onfinish = () => {
 				spoiler.style.overflow = '';
-				spoiler.open = !spoiler.open;
+				spoiler.open = ! spoiler.open;
 			};
 		} else {
 			spoiler.style.overflow = 'hidden';
-			spoiler.style.height = `${summary.offsetHeight}px`;
-			spoiler.open = !spoiler.open;
+			spoiler.style.height = `${ summary.offsetHeight }px`;
+			spoiler.open = ! spoiler.open;
 			const border = parseInt(
 				window
-					.getComputedStyle(summary)
-					.getPropertyValue('border-bottom-width')
-					.slice(0, -2)
+					.getComputedStyle( summary )
+					.getPropertyValue( 'border-bottom-width' )
+					.slice( 0, -2 )
 			);
-			const from = `${summary.offsetHeight + border}px`;
-			const to = `${spoiler.scrollHeight}px`;
+			const from = `${ summary.offsetHeight + border }px`;
+			const to = `${ spoiler.scrollHeight }px`;
 			const ani = spoiler.animate(
 				{
-					height: [from, to],
+					height: [ from, to ],
 				},
 				{
 					duration: 200,
@@ -65,24 +65,24 @@ window.inlineSpoilersAnimation = () => {
 				spoiler.style.height = spoiler.style.overflow = '';
 			};
 		}
-	});
-}
+	} );
+};
 
 // Inline Spoilers 1.5.5 block compatibility
 window.inlineSpoilersBlockAnimationCompatibility = () => {
-	jQuery(function () {
-		jQuery('.spoiler-head').on('click', function (e) {
-			const $this = jQuery(this);
-			let $isExpanded = $this.hasClass('expanded');
-			$this.toggleClass('expanded').toggleClass('collapsed');
+	jQuery( function () {
+		jQuery( '.spoiler-head' ).on( 'click', function ( e ) {
+			const $this = jQuery( this );
+			let $isExpanded = $this.hasClass( 'expanded' );
+			$this.toggleClass( 'expanded' ).toggleClass( 'collapsed' );
 			$isExpanded
-				? $this.next().slideUp('fast')
-				: $this.next().slideDown('fast');
-		});
-	});
-}
+				? $this.next().slideUp( 'fast' )
+				: $this.next().slideDown( 'fast' );
+		} );
+	} );
+};
 
-if (!window.inlineSpoilersAnimationInitialized) {
+if ( ! window.inlineSpoilersAnimationInitialized ) {
 	window.inlineSpoilersAnimation();
 	window.inlineSpoilersBlockAnimationCompatibility();
 }
